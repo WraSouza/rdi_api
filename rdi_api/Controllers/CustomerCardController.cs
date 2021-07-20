00807c267c8494061639a510f0bc46598255fb31
Wladimir Ribeiro de Souza <wladimirsouza@outlook.com>
2021-07-20 15:09:37 -0300

    Adicionar arquivos de projeto.

---
+++ rdi_api/Controllers/CustomerCardController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rdi_api.Controllers
+{
+    public class CustomerCardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+
+        [HttpPost]
+        public void Post(int id,int cardnumber, int cvv)
+        {
+
+        }
+    }
+}
